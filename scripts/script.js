76621c55e18a51621ba894d2cea7598743b1cd8d
cimris <77905334+cimris@users.noreply.github.com>
2021-10-07 23:02:37 -0600

    planned script

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
 // My Scripts
+
+//Prompt User for Rock, Paper, or Scissors
+//Store Answer in a Variable
+//Randomize Computer Choice for Rock, Paper, or Scissors
+//Store Answer in a Variable
+//Compare Player vs Computer Choice
+//if Player picks Rock
+//and Computer pics Rock = Tie
+//and Computer pics Paper = Draw
+//and Computer pics Scissors = Win
+//if Player Pics Paper
+//and Computer pics Rock = Win
+//and Computer pics Paper = Draw
+//and Computer pics Scissors = Lose
+//if Player Picks Scissors
+//and Computer pics Rock = Lose
+//and Computer pics Paper = Win
+//and Computer pics Scissors = Draw
+//keep track of winner and loser points??
+//return result to Player
+
+//repeat for 5 rounds and report score??
