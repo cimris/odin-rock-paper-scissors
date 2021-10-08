d8f1004e5199ef062ab755b53c959e5947885bb1
cimris <77905334+cimris@users.noreply.github.com>
2021-10-07 23:26:00 -0600

    fixed spelling error

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ const choices = ["Rock", "Paper", "Scissors"]
 //Prompt User for Rock, Paper, or Scissors
 //Store Answer in a Variable
 //Randomize Computer Choice for Rock, Paper, or Scissors
-let computerPlay = choices[Math.floor(Math.random() * myArray.length)]
+let computerPlay = choices[Math.floor(Math.random() * choices.length)]
 console.log(computerPlay)
 //Store Answer in a Variable
 //Compare Player vs Computer Choice
