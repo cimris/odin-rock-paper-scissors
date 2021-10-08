6b2b8cbe1ab1667f02ba0eb414473a7f9eee36c7
cimris <77905334+cimris@users.noreply.github.com>
2021-10-08 14:00:06 -0600

    changed rpsRound function

--- scripts/script.js
+++ scripts/script.js
@@ -19,25 +19,25 @@ alert("The computer chose " + computerPlay)
 //Compare Player vs Computer Choice Function for One Game
 function rpsRound() {
   if (userChoice === computerPlay) {
-    alert("Draw")
+    return "Draw"
   } else if (userChoice === "Rock" && computerPlay === "Paper") {
-    alert("You Lose")
+    return "You Lose"
   } else if (userChoice === "Rock" && computerPlay === "Scissors") {
-    alert("You Win")
+    return "You Win"
   } else if (userChoice === "Paper" && computerPlay === "Rock") {
-    alert("You Win")
+    return "You Win"
   } else if (userChoice === "Paper" && computerPlay === "Scissors") {
-    alert("You Lose")
+    return "You Lose"
   } else if (userChoice === "Scissors" && computerPlay === "Rock") {
-    alert("You Lose")
+    return "You Lose"
   } else if (userChoice === "Scissors" && computerPlay === "Paper") {
-    alert("You Win")
+    return "You Win"
   } else {
-    alert("Something went wrong...")
+    return "Something went wrong..."
   }
 }
-
-rpsRound()
+console.log(rpsRound())
+alert(rpsRound())
 //create new function for 5 round game
 //keep track of scores for rounds
 //return score to player each round
