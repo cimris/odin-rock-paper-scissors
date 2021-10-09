e02ba5f701c58ba8ae4aedb30f9ea3086a921487
cimris <77905334+cimris@users.noreply.github.com>
2021-10-08 23:41:21 -0600

    moved choices into round function

--- scripts/script.js
+++ scripts/script.js
@@ -6,21 +6,21 @@ let computerScore = 0
 function userPlay() {
   return window.prompt("Choose Rock, Paper, or Scissors").toLowerCase()
 }
-
-const userChoice = userPlay()
-console.log(userChoice) //delete later
-alert("Your choice is " + userChoice + ".")
 //Randomize Computer Choice for Rock, Paper, or Scissors
 function computerChoice() {
   const computerChoices = ["rock", "paper", "scissors"]
   return computerChoices[Math.floor(Math.random() * computerChoices.length)]
 }
-
-const computerPlay = computerChoice()
-console.log(computerPlay) //delete later
-alert("The computer chose " + computerPlay + ".")
 //Compare Player vs Computer Choice Function for One Game
 function rpsRound() {
+  const userChoice = userPlay()
+  console.log(userChoice) //delete later
+  alert("Your choice is " + userChoice + ".")
+
+  const computerPlay = computerChoice()
+  console.log(computerPlay) //delete later
+  alert("The computer chose " + computerPlay + ".")
+
   if (userChoice === computerPlay) {
     return "Draw"
   } else if (userChoice === "rock" && computerPlay === "paper") {
@@ -45,10 +45,9 @@ function rpsRound() {
     return "Something went wrong..."
   }
 }
-console.log(rpsRound()) //delete later
+alert(rpsRound()) //delete later
 console.log(playerScore)
 console.log(computerScore)
-alert(rpsRound())
 //create new function for 5 round game
 //keep track of scores for rounds
 //return score to player each round
