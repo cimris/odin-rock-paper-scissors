f08177714968244885ed614a8a21bf6be1a12fab
cimris <77905334+cimris@users.noreply.github.com>
2021-10-13 20:53:47 -0600

    changed messages

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,67 @@
 // My Scripts
 //Score
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
 //Prompt User for Rock, Paper, or Scissors
 function userPlay() {
-  return window.prompt("Choose Rock, Paper, or Scissors").toLowerCase()
+  return window.prompt("Choose Rock, Paper, or Scissors").toLowerCase();
 }
 //Randomize Computer Choice for Rock, Paper, or Scissors
 function computerChoice() {
-  const computerChoices = ["rock", "paper", "scissors"]
-  return computerChoices[Math.floor(Math.random() * computerChoices.length)]
+  const computerChoices = ["rock", "paper", "scissors"];
+  return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 //Compare Player vs Computer Choice Function for One Game
 function rpsRound() {
-  const userChoice = userPlay()
-  console.log(userChoice) //delete later
-  alert("Your choice is " + userChoice + ".")
+  const userChoice = userPlay();
+  console.log(userChoice); //delete later
+  alert("Your choice is " + userChoice + ".");
 
-  const computerPlay = computerChoice()
-  console.log(computerPlay) //delete later
-  alert("The computer chose " + computerPlay + ".")
+  const computerPlay = computerChoice();
+  console.log(computerPlay); //delete later
+  alert("The computer chose " + computerPlay + ".");
 
   if (userChoice === computerPlay) {
-    return "Draw"
+    return "Round Draw";
   } else if (userChoice === "rock" && computerPlay === "paper") {
-    computerScore++
-    return "You Lose"
+    computerScore++;
+    return "Round Lost";
   } else if (userChoice === "rock" && computerPlay === "scissors") {
-    playerScore++
-    return "You Win"
+    playerScore++;
+    return "Round Won";
   } else if (userChoice === "paper" && computerPlay === "rock") {
-    playerScore++
-    return "You Win"
+    playerScore++;
+    return "Round Won";
   } else if (userChoice === "paper" && computerPlay === "scissors") {
-    computerScore++
-    return "You Lose"
+    computerScore++;
+    return "Round Lost";
   } else if (userChoice === "scissors" && computerPlay === "rock") {
-    computerScore++
-    return "You Lose"
+    computerScore++;
+    return "Round Lost";
   } else if (userChoice === "scissors" && computerPlay === "paper") {
-    playerScore++
-    return "You Win"
+    playerScore++;
+    return "Round Won";
   } else {
-    return "Something went wrong..."
+    return "Something went wrong this round...";
   }
 }
 //create new function for 5 round game
 function game() {
-  rpsRound()
-  rpsRound()
-  rpsRound()
-  rpsRound()
-  rpsRound()
+  rpsRound();
+  rpsRound();
+  rpsRound();
+  rpsRound();
+  rpsRound();
 
   if (playerScore === computerScore) {
-    return "Draw"
+    return "Draw";
   } else if (playerScore > computerScore) {
-    return "You Win"
+    return "You Win";
   } else if (computerScore > playerScore) {
-    return "You Lose"
+    return "You Lose";
   } else {
-    return "Something went wrong..."
+    return "Something went wrong...";
   }
 }
 
-alert(game())
+alert(game());
